7e6343d404fb4f0c660d9ad3d87bfb6cd1c7ed08
bmcilhenny <bmcilhen@gmail.com>
2018-03-10 21:37:53 -0500

    trying to figure out why path is not working for s3 upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,12 +3,13 @@ Rails.application.configure do
   # AWS
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: ENV['S3_HOST_NAME'],
+    path: ENV["PATH"],
     s3_credentials: {
       bucket: ENV["S3_BUCKET_NAME"],
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      s3_region: ENV["S3_REGION"],
-      s3_host_name: ENV["S3_HOST_NAME"]
+      s3_region: ENV["S3_REGION"]
     }
   }
 
